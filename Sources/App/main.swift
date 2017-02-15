3ebee5fd21c5fe37b6522f7db412b4e48ea37538
Yogendra Shelke <yogendra.shelke@davita.com>
2017-02-15 19:02:58 +0530

    Initial commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,44 @@
+import Vapor
+import VaporPostgreSQL
+
+let drop = Droplet(preparations: [Acronym.self], providers: [VaporPostgreSQL.Provider.self])
+
+drop.get("version") { req in
+        
+        if let db = drop.database?.driver as? PostgreSQLDriver {
+                let version = try db.raw("SELECT version()")
+                return try JSON(node: version)
+        }
+    return "Database connection failed"
+}
+
+drop.get("first") { request in
+        return try JSON(node: Acronym.query().first()?.makeNode())
+}
+
+drop.get("all") { request in
+        return try JSON(node: Acronym.all().makeNode())
+}
+
+drop.post("new") { request in
+        var acronym = try Acronym(node: request.json)
+        try acronym.save()
+        return acronym
+}
+
+drop.get("update") { request in
+        guard var first = try Acronym.query().first(), let long = request.data["long"]?.string else {
+                throw Abort.badRequest
+        }
+        first.long = long
+        try first.save()
+        return first
+}
+
+drop.get("delete-afks") { request in
+        let query = try Acronym.query().filter("short", "AFK")
+        try query.delete()
+        return try JSON(node: Acronym.all().makeNode())
+}
+
+drop.run()
