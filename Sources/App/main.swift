99c7d75f410646af0847f4b9fdc882a5a6a0465e
Yogendra Shelke <yogendra.shelke@davita.com>
2017-02-17 17:58:46 +0530

    Added controller

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,42 +6,7 @@ let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider)
 drop.preparations += Acronym.self
 
-drop.get("version") { req in
-        
-        if let db = drop.database?.driver as? PostgreSQLDriver {
-                let version = try db.raw("SELECT version()")
-                return try JSON(node: version)
-        }
-    return "Database connection failed"
-}
-
-drop.get("first") { request in
-        return try JSON(node: Acronym.query().first()?.makeNode())
-}
-
-drop.get("all") { request in
-        return try JSON(node: Acronym.all().makeNode())
-}
-
-drop.post("new") { request in
-        var acronym = try Acronym(node: request.json)
-        try acronym.save()
-        return acronym
-}
-
-drop.get("update") { request in
-        guard var first = try Acronym.query().first(), let long = request.data["long"]?.string else {
-                throw Abort.badRequest
-        }
-        first.long = long
-        try first.save()
-        return first
-}
-
-drop.get("delete-afks") { request in
-        let query = try Acronym.query().filter("short", "AFK")
-        try query.delete()
-        return try JSON(node: Acronym.all().makeNode())
-}
+let acronyms = AcronymsController()
+drop.resource("acronyms", acronyms)
 
 drop.run()
